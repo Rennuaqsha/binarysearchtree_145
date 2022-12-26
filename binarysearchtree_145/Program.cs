087494fade60e0a20768b50b3b535f8beb78da3a
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-12-26 18:21:04 +0800

    create class node

--- binarysearchtree_145/Program.cs
+++ binarysearchtree_145/Program.cs
@@ -2,5 +2,22 @@
 
 namespace BinarySearchTree
 {
+    class program
+    {
+        /* A node class const 
+         
 
+
+
+        */
+
+        class node
+        {
+            public string info;
+            public node lchidl;
+            public node rchild;
+
+            //constructor for the node class
+        }
+    }
 }
